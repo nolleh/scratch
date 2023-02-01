0b8dc9a5a851718412e3c845e5a2fa1863d870c4
nolleh7707 <nolleh7707@gmail.com>
2023-02-01 23:00:21 +0900

    feat(crawl): test style and tabulate

--- crawl-python/src/crawl.py
+++ crawl-python/src/crawl.py
@@ -2,6 +2,7 @@ import requests as req
 from bs4 import BeautifulSoup
 from pandas import DataFrame
 from IPython.display import display
+from tabulate import tabulate
 
 url = 'https://github.com/nolleh?tap=repositories'
 res = req.get(url, params = {'query' : ''})
@@ -25,6 +26,25 @@ display(df.groupby(['lang'])['lang'].size().reset_index(name='counts'))
 print('********************************')
 print('** group by and sort with count **')
 
-display(df.groupby(['lang'])['lang'].size().sort_values(ascending=False))
+rank = df.groupby(['lang'])['lang'].size().sort_values(ascending=False).reset_index(name='counts')
 
+print(tabulate(rank, headers = 'keys', tablefmt='psql'))
+
+f = open('crawl.html', 'w', newline='')
+cell_hover = {  # for row hover use <tr> instead of <td>
+    'selector': 'td:hover',
+    'props': [('background-color', '#ffffb3')]
+}
+index_names = {
+    'selector': '.index_name',
+    'props': 'font-style: italic; color: darkgrey; font-weight:normal;'
+}
+headers = {
+    'selector': 'th:not(.index_name)',
+    'props': 'background-color: #000066; color: white;'
+}
+
+genhtml = rank.style.set_table_styles([cell_hover, index_names, headers]).to_html()
+wr = f.write(genhtml) 
+f.close()
 
--- crawl-python/src/dataframe.py
+++ crawl-python/src/dataframe.py
@@ -1,7 +1,10 @@
-import pandas
+import pandas as pd
+import numpy as np
 from IPython.display import display
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 
-df = pandas.DataFrame(
+df = pd.DataFrame(
     {"A": [1, 2, 3, 4, 5], "B": [10, 20, 30, 40, 50]}, index=["a", "b", "c", "d", "e"]
 )
 
@@ -18,10 +21,46 @@ display(df['A'])
 
 print('-------\n')
 print('show rearrange')
-rearrange = pandas.DataFrame(df, columns = ['B','A'])
+rearrange = pd.DataFrame(df, columns = ['B','A'])
 display(rearrange)
 
 # print('-------\n')
 # print('select a rows')
 # display(df.loc('a'))
+dict = {'Name' : ['Martha', 'Tim', 'Rob', 'Georgia'],
+        'Maths' : [87, 91, 97, 95],
+        'Science' : [83, 99, 84, 76]}
+df = pd.DataFrame(dict)
+  
+# displaying the DataFrame
+df.style
 
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    color = 'blue' if val > 90 else 'black'
+    return 'color: % s' % color
+  
+# creating a DataFrame
+dict = {'Maths' : [87, 91, 97, 95],
+        'Science' : [83, 99, 84, 76]}
+df = pd.DataFrame(dict)
+  
+# displaying the DataFrame
+df.style.applymap(color_negative_red)
+
+
+print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+
+print('*************')
+ts = pd.Series(np.random.randn(1000), index=pd.date_range("1/1/2000", periods=1000))
+ts = ts.cumsum()
+ts.plot();
+
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list("ABCD"))
+df = df.cumsum()
+plt.figure();
+df.plot();
