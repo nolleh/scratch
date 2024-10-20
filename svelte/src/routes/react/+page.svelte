146258bd5ad3969b690fe709ad7d7ff81b9447f7
nolleh <nolleh7707@gmail.com>
2024-10-20 11:10:43 +0900

    chore: reactive example

---
+++ svelte/src/routes/react/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Child from './Child.svelte';
+
+  let componentTitle = 'child';
+
+  $: data = {
+    title: componentTitle
+  };
+
+  function click() {
+    console.log('clicked');
+    componentTitle = 'changed prop';
+  }
+</script>
+
+<button on:click={click}> click 2 change </button>
+<Child title= "with wrap obj" prop={data.title} />
+<Child title= "without wrap obj" prop={componentTitle} />
---
+++ svelte/src/routes/react/Child.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let prop: string;
+
+  $: bad = () => prop
+  $: good = (t2: string) => {
+    return t2;
+  };
+</script>
+
+<h3>{title}</h3>
+{bad()} <br/>
+{good(prop)}
